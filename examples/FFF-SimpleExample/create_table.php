07980f661bc88496742698c48b1e9a4b49b0fb68
Matthew Hamilton <matt.hamilton@ed.ac.uk>
2022-11-17 14:02:24 +0000

    Create create_table.php

---
+++ examples/FFF-SimpleExample/create_table.php
@@ -0,0 +1,7 @@
+<?php
+// Make table for FFF-SimpleExample
+$home = '/home/'.get_current_user();
+$f3 = require($home.'/AboveWebRoot/fatfree-master/lib/base.php');
+$f3->set('AUTOLOAD','autoload/;'.$home.'/AboveWebRoot/autoload/');
+$db = DatabaseConnection::connect();
+$db->exec("CREATE TABLE IF NOT EXISTS simpleModel (id int NOT NULL, name varchar(128),  colour varchar(128), PRIMARY KEY (id))");
